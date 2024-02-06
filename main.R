5d1e3dc9b0a0941e01d3caed7805a70555f96183
Vaduva-Vlad <vlad.vaduva02@e-uvt.ro>
2024-02-06 14:44:43 +0200

    Changes

--- main.R
+++ main.R
@@ -2,6 +2,10 @@ library(MASS)
 library(ggplot2)
 View(Rabbit)
 
+#Prima faza: Doza din ce in ce mai mare de fenilbiguanida + substanta salina (de control)
+#A doua faza: Doza din ce in ce mai mare de fenilbiguanida + MDL (Medicament de reglare a tensiunii arteriale)
+#Se observa diferentele de tensiune arteriala in ambele faze
+
 control<-subset(Rabbit,Treatment=="Control")
 mdl<-subset(Rabbit,Treatment=="MDL")
 control
@@ -11,10 +15,10 @@ mdl
 line_graph<-function(rabbit,treatment){
   r<-subset(Rabbit,Treatment==treatment & Animal==rabbit)
   r
-  Doza<-r$Dose
-  Schimbari_tensiune<-r$BPchange
-  data<-data.frame(Doza,Schimbari_tensiune)
-  ggplot(data, aes(x=Doza, y=Schimbari_tensiune)) +
+  Dose<-r$Dose
+  BPchange<-r$BPchange
+  data<-data.frame(Dose,BPchange)
+  ggplot(data, aes(x=Dose, y=BPchange)) +
     geom_line()+
     geom_point()
 }
@@ -26,31 +30,26 @@ line_graph("R1","Control")
 line_graph("R1","MDL")
 
 
-boxplot(control$BPchange)
-mediana<-median(control$BPchange)
-mediana
+median(control$BPchange)
 quantile(control$BPchange,prob=c(.25,.5,.75))
+boxplot(control$BPchange)
 mean(control$BPchange)
 
-boxplot(mdl$BPchange)
-mediana<-median(mdl$BPchange)
-mediana
+median(mdl$BPchange)
 quantile(mdl$BPchange,prob=c(.25,.5,.75))
+boxplot(mdl$BPchange)
 mean(mdl$BPchange)
 
-hist(control$BPchange)
-hist(mdl$BPchange)
-hist(Rabbit$Dose)
-
 #Test t pentru esantioane dependente
-#H0: Schimbarile in tensiunea arteriala sunt egale in ambele faze de testare
-#Ha: Schimbarile in tensiunea arteriala sunt diferite in fiecare faza de testare
-test1<-t.test(Rabbit$BPchange[Rabbit$Treatment=="Control"],Rabbit$BPchange[Rabbit$Treatment=="MDL"],alternative="two.sided",conf.level=0.05,paired=TRUE)
+#H0: BPchange este mai mare in prima faza
+#Ha: BPchange este mai mic in prima faza
+test1<-t.test(Rabbit$BPchange[Rabbit$Treatment=="Control"],Rabbit$BPchange[Rabbit$Treatment=="MDL"],alternative="less",conf.level=0.05,paired=TRUE)
 if(test1$p.value<=0.95) print("Respingem ipoteza nula") else  print("Nu respingem ipoteza nula")
 
 correlation=cor(Rabbit$BPchange,Rabbit$Dose)
 correlation
-model=lm(BPchange ~ Dose,data=Rabbit)
+
+model=lm(BPchange ~ Dose, data=Rabbit)
 #Residual standard error = 6.423
 #Mediana valorilor reziduu = -1.534 => Modelul tinde si sa subestimeze, dar si sa supraestimeze.
 model
@@ -58,3 +57,11 @@ summary(model)
 
 plot(BPchange ~ Dose, data=Rabbit)
 abline(model, col = "red")
+
+#MSE - eroarea medie patratica = 39.88163
+mean(model$residuals^2)
+
+v=c(10,25,75,115,150,190)
+new_dose=rep(v, times = 10)
+model=lm(BPchange ~ new_dose, data=Rabbit)
+model
